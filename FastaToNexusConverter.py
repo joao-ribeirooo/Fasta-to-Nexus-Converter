89906cad8df924deee050839d601df91c2374b20
João Ribeiro <97030703+joao-ribeirooo@users.noreply.github.com>
2023-05-17 12:15:15 +0100

    Update FastaToNexusConverter.py
    Addition of the verification of the names in case there is duplicates and correction of minor errors

--- FastaToNexusConverter.py
+++ FastaToNexusConverter.py
@@ -1,29 +1,55 @@
 import sys
 import random
+from collections import Counter
 
 def ReadFile(filename):
     '''
-    Reads the fasta file and inserts into a dictionary the species name and the sequence, respectively
+    Reads the fasta file and inserts into a Dicionario the species name and the sequence, respectively
     Input: Fasta File
-    Output: Dicionario que contem o nome da especie e a respetiva sequencia
+    Output: Dicionario que contem o nome da especie e a respetiva sequencia >IGRA5
     '''
     fastahandle = open(filename, "r")
     Dicionario = {}
     for row in fastahandle:
         if row.startswith(">"):
             name = row.strip()
-            name = name[:99]
+            name = name.replace(">", "")
+            name = name.replace(" ", "_")
             Dicionario[name] = ""
         else:
-            Dicionario[name] += row.strip()       
+            Dicionario[name] += row.strip() 
     fastahandle.close()
     return Dicionario
 
-def VariableNTAX(Dicionario):
-    ntax = len(Dicionario)
-    return ntax
+def VerifyNames(Dicionario):
+    """
+    If there is a name in the dictionary with 99 or more characters it cuts out the one with more than 99 characters and the rest stays the same. 
+    If there are two or more names with 99 characters or more it checks for duplicates.
+    If there are duplicates it will cut them down to character 95 and add a number to differentiate between them.
+    Input : Dictionary 
+    Output : Dictionary with the replacement of the duplicate keys and with more than 99 characters
+    """
+    #longkey = {x for x in Dicionario.keys() if len(x) >= 99}
+    longkey = {}   
+    for key in Dicionario.keys():
+        if len(key) >= 99:
+            longkey[key] = key[:99]
+    Dup = (Counter(longkey.values()) - Counter(set(longkey.values()))).keys()
+    counter = 0    
+    for key, name in longkey.items():
+        if name in Dup:
+            counter += 1
+            newkey = name[:95] + str("%04d" % (counter,))
+            Dicionario[newkey] = Dicionario.pop(key)
+        else:
+            newkey = name[:99]
+            Dicionario[newkey] = Dicionario.pop(key)
+    return Dicionario
 
 def VariableNCHAR(Dicionario):
+    """
+    It will get the NCHAR value by looping through the dictionary
+    """
     lenghts = set()
     for SpeciesName, sequence in Dicionario.items():
         lenghts.add(len(sequence))
@@ -35,17 +61,14 @@ def VariableNCHAR(Dicionario):
     return nchar
 
 def VariableOutgroup(Dicionario, outgroup):
-    outgroup = ">" + outgroup
+    outgroup = outgroup.replace(" ", "_") #in case there is spaces to match the specie name inside the dictionary
+    outgroup = outgroup.replace(">", "") #in case there is > to match the specie name inside the dictionary
     outgroup = outgroup[:99]
     if outgroup not in Dicionario.keys():
         print("The outgroup doesnt seem to be one of the species names inside the fasta file")
         exit()
     else:
-        outgroup = outgroup.replace(">", "")   
-    return outgroup
-
-def VariableNgen(ngen):
-    return ngen
+        return outgroup
 
 def FraseInspiradora():
     FraseRand = ["When there is a true desire in the heart, and that desire is strong, that is when he finds real strength that even he did not know he had!",
@@ -62,18 +85,18 @@ def FraseInspiradora():
     fraseinspiradora = random.choice(FraseRand)
     return fraseinspiradora
 
-
-def WriteFileNexus(nchar, ntax, outgroup, ngen, fraseinspiradora,  Dicionario):
+def WriteFileNexus(nchar, outgroup, ngen, fraseinspiradora,  Dicionario):
     print(f"""[{fraseinspiradora}]
 #NEXUS
 
 BEGIN DATA;
-DIMENSIONS NTAX={ntax} NCHAR={nchar};
+DIMENSIONS NTAX={len(Dicionario)} NCHAR={nchar};
 FORMAT DATATYPE=DNA MISSING=N GAP=-;
 MATRIX""")
     for SpeciesName, sequence in Dicionario.items():
-        SpeciesName = SpeciesName.replace('>', '')
-        print('    '+ SpeciesName + '  ' + sequence)
+        #print('    '+ SpeciesName + '  ' + sequence)
+        print("{: >99} ".format(SpeciesName) + '  ' + sequence)
+
     print("  ;" + "\n" + "  end;")
     print(f'''
 begin mrbayes;
@@ -86,9 +109,8 @@ end;''')
     
 if __name__ == "__main__":
     Dicionario = ReadFile(sys.argv[1])
-    nchar = (VariableNCHAR(Dicionario))
-    ntax = (VariableNTAX(Dicionario))
-    outgroup = VariableOutgroup(Dicionario, sys.argv[2])
-    ngen = VariableNgen(sys.argv[3])
+    Dicionario2 = VerifyNames(Dicionario)
+    nchar = (VariableNCHAR(Dicionario2))
+    outgroup = VariableOutgroup(Dicionario2, sys.argv[2])
     fraseinspiradora= FraseInspiradora()
-    WriteFileNexus(nchar,ntax, outgroup, ngen, fraseinspiradora, Dicionario)
+    WriteFileNexus(nchar, outgroup, sys.argv[3], fraseinspiradora, Dicionario2)
